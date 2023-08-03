c8b829881f577ae6a7516aabd9c8ab43b22f4a5c
shalanoveljan <eljanash@code.edu.az>
2023-08-03 05:40:29 +0400

    contains

---
+++ ContainsMethod/Extension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ContainsMethod
+{
+    internal static class Extension
+    {
+
+        public static bool Contains(this string Sentence, string newSentence)
+        {
+            bool result = false;
+
+            if (Sentence.IndexOf(newSentence) != -1)
+                result = true;
+
+            return result;
+        }
+
+
+    }
+}
---
+++ ContainsMethod/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Program
+{
+  
+
+    public static void Main(string[] args)
+    {
+        string b= "Hello, World!";
+        string a= ", world";
+
+        if (b.ToLower().Contains( a.ToLower()))
+        {
+            Console.WriteLine(" a is found in the b.");
+        }
+        else
+        {
+            Console.WriteLine("a is not found in the b.");
+        }
+    }
+}
+
