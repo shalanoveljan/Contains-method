8f5ba295e07654671cf2d371dfc98af529964126
shalanoveljan <138590101+shalanoveljan@users.noreply.github.com>
2023-08-04 05:54:36 +0400

    Update Extension.cs

--- ContainsMethod/Extension.cs
+++ ContainsMethod/Extension.cs
@@ -8,15 +8,54 @@ namespace ContainsMethod
 {
     internal static class Extension
     {
-
-        public static bool Contains(this string Sentence, string newSentence)
+public static bool CustomContains(this string Sentence, string SearchSentence)
         {
+            //string EmptySentence=string.Empty;
+            int count = 0;
             bool result = false;
+            for(int i=0; i<SearchSentence.Length; i++)
+            {
+                for (int j = 0; j < Sentence.Length; j++)
+                {
+                    if (Sentence[j]== SearchSentence[i])
+                    {
+                        count++;
+                        if (SearchSentence.Length == count)
+                        {
+                            result = true;
+                        }
+                        else
+                        {
+                            
+                            result = false;
+                        }
+                        break;
+                        
 
-            if (Sentence.IndexOf(newSentence) != -1)
-                result = true;
+                    }
+                    else
+                    {
+
+                        continue;
+
+                    }
+
+                  
+
+
+
+                }
+
+
+
+            }
+
+
 
             return result;
+
+
+
         }
 
 
